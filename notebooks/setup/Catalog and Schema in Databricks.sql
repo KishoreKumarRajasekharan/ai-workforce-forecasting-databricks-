d3514447961a0af32b803bea1b68ed16bd039b68
kishore kumar rajasekharan <48342599+KishoreKumarRajasekharan@users.noreply.github.com>
2026-08-17 02:03:25 +0530

    Create Catalog and Schema in Databricks.sql

---
+++ notebooks/setup/Catalog and Schema in Databricks.sql
@@ -0,0 +1,4 @@
+CREATE CATALOG IF NOT EXISTS workforce_ai;
+CREATE SCHEMA IF NOT EXISTS workforce_ai.bronze;
+CREATE SCHEMA IF NOT EXISTS workforce_ai.silver;
+CREATE SCHEMA IF NOT EXISTS workforce_ai.gold;
